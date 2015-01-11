86db7f9c75c7d22a999ca942bb9630d4db945f74
Obside <obsiagae@gmail.com>
2015-01-11 17:05:25 +0100

    insert_message better handled with insert_node_front and nodecmp

--- src/message_linked_list.c
+++ src/message_linked_list.c
@@ -12,8 +12,9 @@ node* create_node(message *msg) {
 }
 
 void free_node(node* n) {
+    /* TODO
     free(n->msg);
-    free(n);
+    free(n);*/
 }
 
 void free_linked_list(node* head) {
@@ -26,29 +27,61 @@ void free_linked_list(node* head) {
     }
 }
 
+/*
+ * Compares two nodes n1 and n2. Nodes are compared according to their message's
+ * timestamp and host_id.
+ *
+ * @param n1 first node to compare
+ * @param n2 second node to compare
+ * @return It returns an integer less than, equal to, or greater than zero if n1
+ * is found, respectively, to be less than, to match, or be greater than n2.
+ */
+int nodecmp(const node* n1, const node* n2) {
+    message *m1 = n1->msg;
+    message *m2 = n2->msg;
+
+    if (m1->timestamp < m2->timestamp) {
+        return -1;
+    }
+    else if (m1->timestamp > m2->timestamp) {
+        return 1;
+    }
+    else {
+        if (m1->host_id < m2->host_id) {
+            return -1;
+        }
+        else if (m1->host_id > m2->host_id) {
+            return 1;
+        }
+        else {
+            return 0;
+        }
+    }
+}
+
+void insert_node_front(node** linked_list, node* new_node) {
+    node* tmp = *linked_list;
+    *linked_list = new_node;
+    new_node->next = tmp;
+}
+
 void insert_message(node** linked_list, message* new_msg) {
     node* new_node = create_node(new_msg);
 
     if (*linked_list == NULL) {
         *linked_list = new_node;
     }
-    else {
+    else if (nodecmp(new_node, *linked_list) <= 0) {
+        insert_node_front(linked_list, new_node);
+    }
+    else if (nodecmp(new_node, *linked_list) > 0) {
         node* cur_node = *linked_list;
         node* next_node = cur_node->next;
 
-        while(next_node != NULL && next_node->msg->timestamp < new_msg->timestamp) {
+        while(next_node != NULL && nodecmp(new_node, next_node) > 0) {
             cur_node = cur_node->next;
             next_node = cur_node->next;
         }
-
-        /* TODO ?
-        next_node = cur_node->next;
-        if (next_node->msg->timestamp == new_msg->timestamp &&
-            next_node->msg->host_id < new_msg->host_id)
-        {
-            cur_node = next_node;
-        }
-        */
 
         new_node->next = next_node;
         cur_node->next = new_node;
@@ -72,7 +105,13 @@ void print_messages_linked_list(node* head) {
     node* cur_node = head;
 
     while (cur_node != NULL) {
-        printf("%d - %s\n", cur_node->msg->timestamp, cur_node->msg->str);
+        printf(
+            "%d - %d - %s\n",
+            cur_node->msg->host_id,
+            cur_node->msg->timestamp,
+            cur_node->msg->str
+        );
+
         cur_node = cur_node->next;
     }
 }
