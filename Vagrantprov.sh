d92386dab6da3dc830a4d733a854158aa36cb683
Obside <obsiagae@gmail.com>
2015-01-12 16:23:10 +0100

    vagrant provisioning : /etc/hosts

--- Vagrantprov.sh
+++ Vagrantprov.sh
@@ -2,3 +2,7 @@
 
 apt-get update
 apt-get install -y git cmake g++ valgrind
+
+echo "192.168.33.10 korra" | sudo tee -a /etc/hosts
+echo "192.168.33.11 katara" | sudo tee -a /etc/hosts
+echo "192.168.33.12 azula" | sudo tee -a /etc/hosts
